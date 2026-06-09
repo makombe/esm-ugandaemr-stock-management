f34844ae5d3ee7ca14092d5c122d4714dddf922c
Laurent Ouma <lawiomosh3@gmail.com>
2026-06-09 10:56:51 +0300

    feat: Add Track and Trace navigation link and styles

--- src/tnt/index.ts
+++ src/tnt/index.ts
@@ -6,3 +6,4 @@ const options = {
 };
 
 export const tntRoot = getAsyncLifecycle(() => import('./track-and-trace.component'), options);
+export const tntNavLink = getAsyncLifecycle(() => import('./components/tnt-nav-link.coomponent'), options);
